07fbad9e6ab97e11f6119fa81c8046406a3f0849
dalvarez <anthrax932yt@gmail.com>
2021-01-02 16:21:33 +0100

    Refactoring code #1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,63 +11,14 @@
   import Presentation from '@/components/Presentation'
   import About from '@/components/About'
   import Services from '@/components/Services'
-  import Portfolio from '../components/Portfolio.vue'
+  import Portfolio from '@/components/Portfolio.vue'
 
   export default {
-  components: {
-    Presentation,
-    About,
-    Services,
-    Portfolio
-  },
-   data() {
-     return {
-       socials: [
-         { icon: 'mdi-facebook', color: 'white' },
-         { icon: 'mdi-twitter', color: 'white' },
-         { icon: 'mdi-linkedin', color: 'white' },
-         { icon: 'mdi-instagram', color: 'white' },
-       ]
-     }
-   }
+    components: {
+      Presentation,
+      About,
+      Services,
+      Portfolio
+    }
   }
 </script>
-
-<style scoped>
-  .home {
-    background-color: var(--v-primary-base);
-  }
-
-  .socials-divider {
-    border: 0;
-    height: 3px;
-    background: rgb(255, 255, 255);
-    width: 50px;
-    margin: auto;
-  }
-
-  .header h1 {
-    color: white;
-  }
-
-  .header h3 {
-    font-size: 1.55em;
-    color: white;
-  }
-
-  .home-title {
-    margin-top: -10em;
-  }
-
-  .home-title h1 {
-    font-size: 3em;
-    letter-spacing: 0.1em;
-  }
-  .home-title h2 {
-    margin-top: 1em;
-    font-weight: 700;
-    font-size: 1rem;
-    line-height: 1.5;
-    letter-spacing: 0.125em;
-  }
-</style>
